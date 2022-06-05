3976ae31c6802838db8c8756d15fb671692fbca8
JimmyCiculs <96069122+Ciculs@users.noreply.github.com>
2022-06-05 16:50:04 +0700

    thay doi xiu

--- js/myJs.js
+++ js/myJs.js
@@ -101,7 +101,7 @@ $('#yes').click(function() {
         width: 900,
         padding: '3em',
         html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("img/input-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy2.gif")
